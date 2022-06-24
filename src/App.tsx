0609398ddb9b34b3b7290b133974785327600d61
JrzenonDev <jrobertoonb@gmail.com>
2022-06-24 01:53:03 -0300

    mout list with array lessons

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,30 @@
+import { gql, useQuery } from "@apollo/client"
+
+const GET_LESSONS_QUERY = gql`
+  query {
+    lessons {
+      id
+      title
+    }
+  }
+`
+
+interface Lesson {
+  id: String,
+  title: String
+}
+
 function App() {
+
+  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
 
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <ul>  
+        {data?.lessons.map(lesson => {
+          return <li key={lesson.id}>{lesson.title}</li>
+        })}
+      </ul>
     </div>
   )
 }
