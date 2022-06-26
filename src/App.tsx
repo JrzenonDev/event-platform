99fbe63f5b75d7629fc5a2ac24c5b529ecf6fa63
JrzenonDev <jrobertoonb@gmail.com>
2022-06-26 18:10:02 -0300

    create query, use apollo/client

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
-import { gql, useQuery } from "@apollo/client"
-
-const GET_LESSONS_QUERY = gql`
-  query {
-    lessons {
-      id
-      title
-    }
-  }
-`
-
-interface Lesson {
-  id: String,
-  title: String
-}
+import { Header } from "./components/Header"
 
 function App() {
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS_QUERY)
-
   return (
     <div className="App">
-      <ul>  
-        {data?.lessons.map(lesson => {
-          return <li key={lesson.id}>{lesson.title}</li>
-        })}
-      </ul>
+     <Header />
     </div>
   )
 }
