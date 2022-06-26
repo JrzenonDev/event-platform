b1a9650e16762f31b81a39af9dd4e30dc023a3d5
JrzenonDev <jrobertoonb@gmail.com>
2022-06-26 19:04:51 -0300

    add page event in App

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,9 @@
-import { Header } from "./components/Header"
+import { Event } from "./pages/Event"
 
 function App() {
   return (
     <div className="App">
-     <Header />
+     <Event />
     </div>
   )
 }
